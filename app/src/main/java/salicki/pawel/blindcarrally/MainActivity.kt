50d2c2689c7f4536eec654cabd933725c889a799
Paweł Salicki <psalicki@evertop.pl>
2020-09-13 11:48:32 +0200

    Initial commit

---
+++ app/src/main/java/salicki/pawel/blindcarrally/MainActivity.kt
@@ -0,0 +1,11 @@
+package salicki.pawel.blindcarrally
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
